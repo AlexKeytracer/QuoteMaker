ca8a590cddd9729950ae6b522cc1f332566d3a56
Alex <alex.c@keytracer.com>
2016-06-27 01:34:23 -0700

    New Initial Commit

---
+++ src/quotemaker/Info.java
@@ -0,0 +1,240 @@
+package quotemaker;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author Alex
+ */
+public final class Info {
+    
+    public static final Map<String, String> IDS = new HashMap<>();
+    public static final Map<String, Integer> PRICES = new HashMap<>();
+    
+    public static void initialize() {
+        
+        IDS.put("FLEXX 6U Cabinet Steel Door - 2 Panels","00358.000");
+        IDS.put("FLEXX 12U Cabinet Steel Door - 4 Panels","00360.000");
+        IDS.put("FLEXX 24U Cabinet Steel Door - 8 Panels","00362.000");
+        IDS.put("FLEXX II 3U Cabinet Green with Lexan Door","00532.000");
+        IDS.put("FLEXX 6U Cabinet Clear Door - 2 Panels","00357.000");
+        IDS.put("FLEXX 12U Cabinet Clear Door - 4 Panels","00359.000");
+        IDS.put("FLEXX 24U Cabinet Clear Door - 8 Panels","00361.000");
+        IDS.put("proxSafe flexx stand for 24U","00443.000");
+        IDS.put("proxSafe flexx II housing 36 U with steel door and clear window panel","00544.00");
+        IDS.put("proxSafe flexx II housing 36U with steel door","00543.00");
+        IDS.put("3U 8 Key Module containing 8 Key tags, rings & seals","00386.000");
+        IDS.put("3U 16 Key Module containing 16 Key tags, rings & seals","00355.000");
+        IDS.put("6U 16 Key Module containing 16 Key tags, rings & seals","00356.000");
+        IDS.put("3U 32 Double Density Module containing 32 Key tags, rings & seals","00468.000");
+        IDS.put("Blind Panels for future expansion","00367.000");
+        IDS.put("proxSafe Terminal C4","00387.000");
+        IDS.put("proxSafe Touch Terminal C6 with 5.7\" display","00495.000");
+        IDS.put("proxSafe terminal fixture for C1/C2 and C6 suitable for flexx II and maxx systems","00593.000");
+        IDS.put("proxSafe terminal fixture for C1/C2 and C6 suitable for locker and racx systems","00604.000");
+        IDS.put("proxSafe terminal fixture for C4 terminal","00446.000");
+        IDS.put("proxSafe terminal fixture for C4 suitable for locker or racx systems","00445.000");
+        IDS.put("Power supply","00315.000");
+        IDS.put("Desealing tool","00311.000");
+        IDS.put("DigiConnect TCP/IP Ethernet Module","1012253");
+        IDS.put("Card Reader - *System will not be ordered until we have sample card in office","TBD");
+        IDS.put("Biometric Finger Print Reader","Finger");
+        IDS.put("Biometric Facial Reader","Face");
+        IDS.put("Kiosk","Kiosk");
+        IDS.put("Mechanical Key Cylinders--Special Keying to Match--All Lockers","00694.000");
+        IDS.put("Mechanical Key Cylinders--Special Keying to Match--3U, 6U and 12U Flexx","00695.000");
+        IDS.put("Mechanical Key Cylinders--Special Keying to Match--24U and 36U Flexx","00696.000");
+        IDS.put("Key Ring 25mm (10)","00314.000");
+        IDS.put("Key Ring 60mm (5 rings 10 grey seals)","00368.000");
+        IDS.put("Key Ring 90mm (5 rings 10 grey seals)","00369.000");
+        IDS.put("Key Tag single Black","00307.000S");
+        IDS.put("Key Tag Kit 16 units","00307.000");
+        IDS.put("Grey Seals","00500.000");
+        IDS.put("Green Serialised Seal","00501.000");
+        IDS.put("Key Card Fob","CardFob");
+        IDS.put("Software proxSafe Commander V4.x basic Per Terminal License code","00555.001");
+        IDS.put("software proxSafe Commander V4.x Additional Terminal","00555.500");
+        IDS.put("FLM - Fleet Management Module Per Terminal fleet module","00556.001");
+        IDS.put("KRV - KeyTag Reservation Module","00557.001");
+        IDS.put("CSO - High Security Module Per Terminal email alerts for late keys, dual/triple authorization, etc","00558.001");
+        IDS.put("EXO - Extended Options Module Per Terminal","00559.001");
+        IDS.put("SEC - Secure Transaction Module Per Terminal","00560.001");
+        IDS.put("HST - High Secure Transaction Module Per Terminal","00561.001");
+        IDS.put("EXR - Extended Reports Module Per Terminal","00562.001");
+        IDS.put("KTR - Keytag Time Restriction Module Per Terminal","00563.001");
+        IDS.put("ERV - Easy Reservation Module Per Terminal","00564.001");
+        IDS.put("TIL - Transaction Logging Module Per Terminal","00565.001");
+        IDS.put("GRO - Group Release Options Module Per Terminal","00566.001");
+        IDS.put("License Check Module Per Terminal","00567.001");
+        IDS.put("Web Services WBS","00378.000");
+        IDS.put("Importer IMP","00458.00");
+        IDS.put("Lightweight Directory Access Protocol LDAP","00603.000");
+        IDS.put("Lenel OnGuard Interface LNL Requires WBS in addition","00506.000");
+        IDS.put("Software House CCURE 9000 Interface SWH Requires WBS in addition","00550.000");
+        IDS.put("Object Linking & Embedding for Process Control OPC","00457.000");
+        IDS.put("Alarmer Module Per Terminal","00568.001");
+        IDS.put("FLM each Additional Terminal","00556.500");
+        IDS.put("KRV Each additional terminal","00557.500");
+        IDS.put("CSO Each Additional Terminal","00558.500");
+        IDS.put("EXO Each Additional Terminal","00559.500");
+        IDS.put("SEC Each Additional Terminal","00560.500");
+        IDS.put("HST Each Additional Terminal","00561.500");
+        IDS.put("EXR Each Additional Terminal","00562.500");
+        IDS.put("KTR Each Additional Terminal","00563.500");
+        IDS.put("ERV Each Additional Terminal","00564.500");
+        IDS.put("Transaction Logging Each Additional Terminal","00565.500");
+        IDS.put("GRO Each Additional Terminal","00566.500");
+        IDS.put("LCK Each Additional Terminal","00567.500");
+        IDS.put("ALM Each Additional Terminal","00568.500");
+        IDS.put("proxSafe locker S12-8 TO (incl. mounting set and standard base)","00670.000");
+        IDS.put("proxSafe locker S12-10 O (incl. mounting set and standard base)","00633.000");
+        IDS.put("proxSafe locker S12-10 TO (incl. mounting set and standard base)","00634.000");
+        IDS.put("proxSafe locker S12 O (incl. mounting set and standard base)","00635.000");
+        IDS.put("proxSafe locker S14-11 TO (incl. mounting set and standard base)","00637.000");
+        IDS.put("proxSafe locker S14 O (incl. mounting set and standard base)","00636.000");
+        IDS.put("proxSafe locker M12 O (incl. mounting set)","00693.000");
+        IDS.put("proxSafe weapon locker setup per weapon type","00454.000");
+        IDS.put("proxSafe weapon transponder","00460.000");
+        IDS.put("proxSafe locker L4 O (incl. mounting set)","00578.000");
+        IDS.put("proxSafe locker L6 O (incl. mounting set)","00618.000");
+        IDS.put("proxSafe locker L8 O (incl. mounting set)","00619.000");
+        IDS.put("proxSafe RFID proxCylinder for 1 compartment","00690.000");
+        IDS.put("proxSafe weapon fixture & RFID for 1 compartment","00689.000");
+        IDS.put("proxSafe locker S and M side panels, right and left","00691.000");
+        IDS.put("proxSafe locker L side panels, right and left","00697.000");
+        IDS.put("proxSafe Service pack with power supply, back-up battery and backplane","00430.000");
+        IDS.put("KT Active RFID Alarm Reader","RT-Reader");
+        IDS.put("Key Tracer Alarm Reader Amber Strobe","PR-STLIAM");
+        IDS.put("Key Tracer Alarm Reader Siren","PR-PS200");
+        IDS.put("Additional WLM Wireless Location Module","WLM");
+        IDS.put("Key Tracer Active Key Tag (2 year warranty; $20 credit when returned)","RT-Key Tag");
+        IDS.put("KeyAlert Software License 12 months Yearly fee - 1st reader","AnnSupp");
+        IDS.put("KeyAlert Software License, Annual Support - each additional reader","Reader");
+        IDS.put("Grey Seals","00500.000");
+        
+        
+        
+        PRICES.put("FLEXX 6U Cabinet Steel Door - 2 Panels", 2725);
+        PRICES.put("FLEXX 12U Cabinet Steel Door - 4 Panels", 3160);
+        PRICES.put("FLEXX 24U Cabinet Steel Door - 8 Panels", 4620);
+        PRICES.put("FLEXX II 3U Cabinet Green with Lexan Door", 2050);
+        PRICES.put("FLEXX 6U Cabinet Clear Door - 2 Panels", 2895);
+        PRICES.put("FLEXX 12U Cabinet Clear Door - 4 Panels", 3245);
+        PRICES.put("FLEXX 24U Cabinet Clear Door - 8 Panels", 5260);
+        PRICES.put("proxSafe flexx stand for 24U", 760);
+        PRICES.put("proxSafe flexx II housing 36 U with steel door and clear window panel", 7575);
+        PRICES.put("proxSafe flexx II housing 36U with steel door", 7050);
+        PRICES.put("3U 8 Key Module containing 8 Key tags, rings & seals", 1430);
+        PRICES.put("3U 16 Key Module containing 16 Key tags, rings & seals", 1595);
+        PRICES.put("6U 16 Key Module containing 16 Key tags, rings & seals", 1595);
+        PRICES.put("3U 32 Double Density Module containing 32 Key tags, rings & seals", 2255);
+        PRICES.put("Blind Panels for future expansion", 95);
+        PRICES.put("proxSafe Terminal C4", 1475);
+        PRICES.put("proxSafe Touch Terminal C6 with 5.7\" display", 3625);
+        PRICES.put("proxSafe terminal fixture for C1/C2 and C6 suitable for flexx II and maxx systems", 140);
+        PRICES.put("proxSafe terminal fixture for C1/C2 and C6 suitable for locker and racx systems", 160);
+        PRICES.put("proxSafe terminal fixture for C4 terminal", 140);
+        PRICES.put("proxSafe terminal fixture for C4 suitable for locker or racx systems", 160);
+        PRICES.put("Power supply", 390);
+        PRICES.put("Desealing tool", 133);
+        PRICES.put("DigiConnect TCP/IP Ethernet Module", 649);
+        PRICES.put("Card Reader - *System will not be ordered until we have sample card in office", 655);
+        PRICES.put("Biometric Finger Print Reader", 995);
+        PRICES.put("Biometric Facial Reader", 1295);
+        PRICES.put("Kiosk", 2900);
+        PRICES.put("Mechanical Key Cylinders--Special Keying to Match--All Lockers", 105);
+        PRICES.put("Mechanical Key Cylinders--Special Keying to Match--3U, 6U and 12U Flexx", 200);
+        PRICES.put("Mechanical Key Cylinders--Special Keying to Match--24U and 36U Flexx", 300);
+        PRICES.put("Key Ring 25mm (10)", 50);
+        PRICES.put("Key Ring 60mm (5 rings 10 grey seals)", 89);
+        PRICES.put("Key Ring 90mm (5 rings 10 grey seals)", 95);
+        PRICES.put("Key Tag single Black", 40);
+        PRICES.put("Key Tag Kit 16 units", 260);
+        PRICES.put("Grey Seals", 59);
+        PRICES.put("Green Serialised Seal", 175);
+        PRICES.put("Key Card Fob", 133);
+        PRICES.put("Software proxSafe Commander V4.x basic Per Terminal License code", 450);
+        PRICES.put("software proxSafe Commander V4.x Additional Terminal", 190);
+        PRICES.put("FLM - Fleet Management Module Per Terminal fleet module", 375);
+        PRICES.put("KRV - KeyTag Reservation Module", 249);
+        PRICES.put("CSO - High Security Module Per Terminal email alerts for late keys, dual/triple authorization, etc", 495);
+        PRICES.put("EXO - Extended Options Module Per Terminal", 250);
+        PRICES.put("SEC - Secure Transaction Module Per Terminal", 250);
+        PRICES.put("HST - High Secure Transaction Module Per Terminal", 165);
+        PRICES.put("EXR - Extended Reports Module Per Terminal", 165);
+        PRICES.put("KTR - Keytag Time Restriction Module Per Terminal", 165);
+        PRICES.put("ERV - Easy Reservation Module Per Terminal", 165);
+        PRICES.put("TIL - Transaction Logging Module Per Terminal", 165);
+        PRICES.put("GRO - Group Release Options Module Per Terminal", 165);
+        PRICES.put("License Check Module Per Terminal", 250);
+        PRICES.put("Web Services WBS", 1350);
+        PRICES.put("Importer IMP", 225);
+        PRICES.put("Lightweight Directory Access Protocol LDAP", 2025);
+        PRICES.put("Lenel OnGuard Interface LNL Requires WBS in addition", 1350);
+        PRICES.put("Software House CCURE 9000 Interface SWH Requires WBS in addition", 2025);
+        PRICES.put("Object Linking & Embedding for Process Control OPC", 3983);
+        PRICES.put("Alarmer Module Per Terminal", 165);
+        PRICES.put("FLM each Additional Terminal", 210);
+        PRICES.put("KRV Each additional terminal", 210);
+        PRICES.put("CSO Each Additional Terminal", 250);
+        PRICES.put("EXO Each Additional Terminal", 125);
+        PRICES.put("SEC Each Additional Terminal", 125);
+        PRICES.put("HST Each Additional Terminal", 85);
+        PRICES.put("EXR Each Additional Terminal", 58);
+        PRICES.put("KTR Each Additional Terminal", 58);
+        PRICES.put("ERV Each Additional Terminal", 59);
+        PRICES.put("Transaction Logging Each Additional Terminal", 85);
+        PRICES.put("GRO Each Additional Terminal", 59);
+        PRICES.put("LCK Each Additional Terminal", 125);
+        PRICES.put("ALM Each Additional Terminal", 85);
+        PRICES.put("proxSafe locker S12-8 TO (incl. mounting set and standard base)", 5760);
+        PRICES.put("proxSafe locker S12-10 O (incl. mounting set and standard base)", 5825);
+        PRICES.put("proxSafe locker S12-10 TO (incl. mounting set and standard base)", 6050);
+        PRICES.put("proxSafe locker S12 O (incl. mounting set and standard base)", 6050);
+        PRICES.put("proxSafe locker S14-11 TO (incl. mounting set and standard base)", 6150);
+        PRICES.put("proxSafe locker S14 O (incl. mounting set and standard base)", 6160);
+        PRICES.put("proxSafe locker M12 O (incl. mounting set)", 6589);
+        PRICES.put("proxSafe weapon locker setup per weapon type", 635);
+        PRICES.put("proxSafe weapon transponder", 17);
+        PRICES.put("proxSafe locker L4 O (incl. mounting set)", 8745);
+        PRICES.put("proxSafe locker L6 O (incl. mounting set)", 9295);
+        PRICES.put("proxSafe locker L8 O (incl. mounting set)", 9889);
+        PRICES.put("proxSafe RFID proxCylinder for 1 compartment", 65);
+        PRICES.put("proxSafe weapon fixture & RFID for 1 compartment", 210);
+        PRICES.put("proxSafe locker S and M side panels, right and left", 635);
+        PRICES.put("proxSafe locker L side panels, right and left", 825);
+        PRICES.put("proxSafe Service pack with power supply, back-up battery and backplane", 1375);
+        PRICES.put("KT Active RFID Alarm Reader", 850);
+        PRICES.put("Key Tracer Alarm Reader Amber Strobe", 100);
+        PRICES.put("Key Tracer Alarm Reader Siren", 100);
+        PRICES.put("Additional WLM Wireless Location Module", 200);
+        PRICES.put("Key Tracer Active Key Tag (2 year warranty; $20 credit when returned)", 65);
+        PRICES.put("KeyAlert Software License 12 months Yearly fee - 1st reader", 900);
+        PRICES.put("KeyAlert Software License, Annual Support - each additional reader", 250);
+        PRICES.put("Grey Seals", 59);
+        
+        sanityCheck();
+    }
+    
+    public static void sanityCheck() {
+        
+        for(String product : IDS.keySet()) {
+            
+            if(PRICES.containsKey(product) == false) {
+                System.out.println("ERROR: ID for \"" + product +"\" exists but no price!");
+            }
+            
+        }
+        
+        for(String product : PRICES.keySet()) {
+            
+            if(IDS.containsKey(product) == false) {
+                System.out.println("ERROR: Price for \"" + product +"\" exists but no ID!");
+            }
+            
+        }
+        
+    }
+    
+}
---
+++ src/quotemaker/QuoteManager.java
@@ -0,0 +1,30 @@
+package quotemaker;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ *
+ * @author Alex
+ */
+public class QuoteManager {
+    
+    public final MainFrame frame;
+    public String proxSafeCabinet = "";
+    
+    public Map<String, Integer> proxSafePanels = new HashMap<>();
+    public Map<String, Integer> proxSafeTerminals = new HashMap<>();
+    
+    public QuoteManager (final MainFrame frame) {
+        
+        this.frame = frame;
+        
+    }
+    
+    public void run() {
+        
+        
+        
+    }
+    
+}
